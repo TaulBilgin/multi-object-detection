cdb579986cde8596172505e9e82142a3af084a4e
Ahmet Bilgin <138232584+TaulBilgin@users.noreply.github.com>
2025-02-11 17:06:57 +0300

    Add files via upload

---
+++ create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(train_data='C:\\code\\a-PyTorch-Tutorial-to-Object-Detection-master\\finis_data\\train',
+                      test_data='C:\\code\\a-PyTorch-Tutorial-to-Object-Detection-master\\finis_data\\test',
+                      output_folder='./')
