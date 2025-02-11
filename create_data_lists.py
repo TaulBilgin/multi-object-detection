0d01fb53b21536c3ff19d61be1c9bae50df44b43
Ahmet Bilgin <138232584+TaulBilgin@users.noreply.github.com>
2025-02-11 17:09:37 +0300

    Update create_data_lists.py

--- create_data_lists.py
+++ create_data_lists.py
@@ -4,3 +4,8 @@ if __name__ == '__main__':
     create_data_lists(train_data='C:\\code\\a-PyTorch-Tutorial-to-Object-Detection-master\\finis_data\\train',
                       test_data='C:\\code\\a-PyTorch-Tutorial-to-Object-Detection-master\\finis_data\\test',
                       output_folder='./')
+# write your data path like
+"""if __name__ == '__main__':
+    create_data_lists(train_data='C:\\code\\...',
+                      test_data='C:\\code\\...',
+                      output_folder='./')"""
